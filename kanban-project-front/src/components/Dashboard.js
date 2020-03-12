be614d2810ba2de92b55880d2deab5484e7e01d4
mohammed douhi <mr.douhi.med@gmail.com>
2020-03-12 09:14:15 +0100

    Adding dashboard, navbar and project item html template

--- kanban-project-front/src/components/Dashboard.js
+++ kanban-project-front/src/components/Dashboard.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import ProjectItem from "./project/ProjectItem";
+import Header from "./layouts/header";
 
 class Dashboard extends Component {
   state = {};
   render() {
-    return <div>Dashboard</div>;
+    return (
+      <div>
+        <Header />
+        <div className="container">
+          <button className="btn btn-outline-primary">New Project</button>
+          <hr />
+          <ProjectItem />
+        </div>
+      </div>
+    );
   }
 }
 
--- kanban-project-front/src/components/project/ProjectItem.js
+++ kanban-project-front/src/components/project/ProjectItem.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
 
-
-class ProjectItem extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
+class ProjectItem extends React.Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <div className="row">
+          <div className="col-4">
+            <div className="card text-center mb-2">
+              <div className="card-header">Featured</div>
+              <div className="card-body">
+                <h5 className="card-title">Special title treatment</h5>
+                <p className="card-text">
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </p>
+                <a href="#" className="btn btn-success btn-sm m-2">
+                  Infos
+                </a>
+                <a href="#" className="btn btn-info btn-sm m-2">
+                  Update
+                </a>
+                <a href="#" className="btn btn-danger btn-sm m-2">
+                  Delete
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default ProjectItem;
