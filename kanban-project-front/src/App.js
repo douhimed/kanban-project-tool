e57a17ed2aa25c400bbf12f9d306ef6d44796d41
mohammed douhi <mr.douhi.med@gmail.com>
2020-03-12 14:30:02 +0100

    Setting up redux

--- kanban-project-front/src/App.js
+++ kanban-project-front/src/App.js
@@ -5,17 +5,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/layouts/header";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NewProject from "./components/project/NewProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends React.Component {
   state = {};
   render() {
     return (
-      <Router>
-        <Header />
-        <Route exact path="/projects" component={Dashboard} />
-        <Route exact path="/projects/new" component={NewProject} />
-        <Route exact path="/" component={Dashboard} />
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Header />
+          <Route exact path="/projects" component={Dashboard} />
+          <Route exact path="/projects/new" component={NewProject} />
+          <Route exact path="/" component={Dashboard} />
+        </Router>
+      </Provider>
     );
   }
 }
