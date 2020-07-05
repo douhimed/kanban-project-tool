a76c999450f7d9c357ebe95b9d2885615d8e88d4
douhimed <mr.douhi.med@gmail.com>
2020-07-05 21:15:18 +0100

    config ec

--- kanban-project/src/main/java/org/adex/security/SecurityConfiguration.java
+++ kanban-project/src/main/java/org/adex/security/SecurityConfiguration.java
@@ -45,7 +45,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors()
+        http
+                .cors()
                 .and()
                 .csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEntryPoint)
